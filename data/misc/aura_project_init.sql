73f4365655f9920812f5de3fa299d924fcdfde5d
Aura Marin <aurasarahimg@gmail.com>
2022-01-18 22:37:21 -0600

    Initial commit of all files to the repository

---
+++ data/misc/aura_project_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS aura_project_db;
+CREATE DATABASE aura_project_db;
+
+\c aura_project_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
